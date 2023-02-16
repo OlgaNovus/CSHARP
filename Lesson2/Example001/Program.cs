515f78f90abfff21830cdb61ffd8bbd5402c93a6
Olga <o.cravets13@yandex.ru>
2023-02-16 23:18:48 +0100

    Добавленно решение задачи 10

---
+++ Lesson2/Example001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine("Введи трёхзначное число");
+
+String? readLine = Console.ReadLine();
+
+if (int.TryParse(readLine, out _))
+{
+    Console.WriteLine("Вторая цифра этого числа "+readLine[1]);
+}
+else
+{
+    Console.WriteLine("Буковки не число!");
+}
