e380bd15abdea460eb0ccbbf0671f3015553b313
Olga <o.cravets13@yandex.ru>
2023-02-19 13:15:15 +0100

    Решение задачи 15

---
+++ Lesson2/Example003/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+while (true)
+{
+    string inputStr = RequestInput();
+
+    int digit;
+    if (IsAndToDigit(inputStr, out digit))
+    {
+        if (IsDayOfWeek(digit))
+        {
+            if (IsWeekend(digit))
+            {
+                Console.WriteLine("Поздравляем! Это выходной :)");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Это не выходной!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Это не день недели!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это не число!");
+    }
+}
+
+//===================================================
+string RequestInput()
+{
+    Console.WriteLine("Введите число, которое день недели");
+    return Console.ReadLine();
+}
+
+bool IsAndToDigit(string str, out int digit)
+{
+    return int.TryParse(str, out digit);
+}
+
+bool IsDayOfWeek(int day)
+{
+    return (day > 0 && day <= 7);
+}
+
+bool IsWeekend(int day)
+{
+    return (day == 6 || day == 7);
+}
