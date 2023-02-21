5f16aca13dec4527dab916591a9f58ae0e01e803
Olga <o.cravets13@yandex.ru>
2023-02-21 20:43:29 +0100

    Решение дадачи 19

---
+++ Lesson3/Example001/Program.cs
@@ -0,0 +1,60 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Введите пятизначное число");
+    string str1 = Console.ReadLine();
+
+    bool isValidLength = ValidLength(str1);
+
+    if (isValidLength)
+    {
+        bool isDigit = int.TryParse(str1, out _);
+
+        if (isDigit)
+        {
+            IsPalindrom(str1);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введенное значение не является числом");
+        }
+    }
+}
+
+
+
+static bool ValidLength(string outDigit)
+{
+    if (outDigit.Length == 5)
+    {
+        return true;
+    }
+    Console.WriteLine("Введенное число не пятизначное");
+    return false;
+}
+
+static void IsPalindrom(string number)
+{
+    int count = 0;
+    int length = number.Length;
+
+    for (int i = 0; i < (length / 2); i++)
+    {
+        int j = (length - 1) - i;
+
+        if (number[i] == number[j])
+        {
+            count++;
+        }
+    }
+
+    if (count == length / 2)
+    {
+        Console.WriteLine("Число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число - не палиндром");
+    }
+}
