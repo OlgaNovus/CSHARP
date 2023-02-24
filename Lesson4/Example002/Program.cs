3ac9edf5cb348275bbf52341cd4d725889ad261e
Olga <o.cravets13@yandex.ru>
2023-02-24 20:29:31 +0100

    Решение задачи 27

---
+++ Lesson4/Example002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе
+
+int usrInpt = readNumber();
+Console.WriteLine("Сумма всех чисел в числе " + usrInpt + " = " + SumNumber(usrInpt));
+
+static int readNumber()
+{
+    int result;
+    bool isDigit;
+    do
+    {
+        Console.Write("Введите число: ");
+        string userInput = Console.ReadLine();
+        isDigit = int.TryParse(userInput, out result);
+        if (!isDigit)
+        {
+            Console.WriteLine("Введенное значение - не число. Повторите ввод.");
+        }
+    } while (!isDigit);
+
+    return result;
+}
+
+static int SumNumber(int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
