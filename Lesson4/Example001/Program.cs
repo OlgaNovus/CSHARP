7eaeac2e4579c4d90b554ff89e6257a0ac299e7b
Olga <o.cravets13@yandex.ru>
2023-02-24 17:23:58 +0100

    Решение задачи 25

---
+++ Lesson4/Example001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (NaturalNumber(numberB))
+{
+    int exponentiation = Exponentiation(numberA, numberB);
+    Console.WriteLine("Число " + numberA + " в степени " + numberB + " = " + exponentiation);
+}
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+static bool NaturalNumber(int numberB)
+{
+    if (numberB > 0)
+    {
+        return true;
+    }
+    Console.WriteLine("Показатель не должен быть меньше 0");
+    return false;
+}
