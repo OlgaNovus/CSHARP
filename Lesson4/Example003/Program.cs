4910e3faa1cbf97f38cf91aa53e87f145220cd0b
Olga <o.cravets13@yandex.ru>
2023-02-27 00:04:15 +0100

    Решение задачи 29

---
+++ Lesson4/Example003/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+
+Console.WriteLine("Задайте длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+int[] filledArray = FillArray(array);
+PrintArray(filledArray);
+
+static void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        Console.Write("{0} ", arrayForPrint[i]);
+    }
+}
+
+static int[] FillArray(int[] arrayOfNumbers)
+{
+    int size = arrayOfNumbers.Length;
+    Random randomGenerator = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arrayOfNumbers[i] = randomGenerator.Next(1, 100);
+    }
+
+    return arrayOfNumbers;
+}
