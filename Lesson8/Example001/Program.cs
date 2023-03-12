4f4d08470548b7f1831fa433b884ac81e3f42ac7
Olga <o.cravets13@yandex.ru>
2023-03-12 16:44:29 +0100

    Решение задачи 54 и 56

---
+++ Lesson8/Example001/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+
+Console.WriteLine("Введите количество строк");
+int linesArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columnsArray = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesArray, columnsArray];
+
+FillArrayRandom(numbers);
+PrintArray(numbers);
+SortDownArray(numbers);
+Console.WriteLine();
+PrintArray(numbers);
+
+
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,3}", array[i, j]));
+
+            if (j != array.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
+
+static void SortDownArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int replace = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = replace;
+                }
+            }
+        }
+    }
+}
+
+void FillArrayRandom(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(0, 100)) / 10;
+        }
+    }
+}
---
+++ Lesson8/Example002/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов
+
+Console.WriteLine("Введите количество строк");
+int linesArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columnsArray = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesArray, columnsArray];
+FillArrayRandom(numbers);
+PrintArray(numbers);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {RowNumMinSum(numbers)}");
+
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,3}", array[i, j]));
+
+            if (j != array.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void FillArrayRandom(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(0, 100)) / 10;
+        }
+    }
+}
+
+static int RowNumMinSum(int[,] numbers)
+{
+    int dimensionLength = numbers.GetLength(0);
+    int[] result = new int[dimensionLength];
+
+    int leastSum = int.MaxValue;
+    int rowLeastSum = 0;
+
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.GetLength(1); i++)
+        {
+            sum = sum + numbers[j, i];
+        }
+
+        if (sum < leastSum)
+        {
+            leastSum = sum;
+            rowLeastSum = j;
+        }
+    }
+
+    return rowLeastSum;
+}
