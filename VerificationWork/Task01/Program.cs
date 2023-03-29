5206bb4fa55f8ae17b88d9b1a527ccd3f4a6b3f1
Olga <o.cravets13@yandex.ru>
2023-03-29 23:18:43 +0200

    Решение задачи

---
+++ VerificationWork/Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которы меньше либо равна 3 символа.
+
+Console.Write("Задайте значения внутри массива через запятую: ");
+string[] inputArray = Console.ReadLine().Split(",");
+string[] secondArray = new string[] { };
+
+PrintArray(inputArray);
+FindShortWords(inputArray, ref secondArray);
+Console.WriteLine("\n");
+PrintArray(secondArray);
+
+static void FindShortWords(string[] inputArray, ref string[] outputArray)
+{
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            Array.Resize(ref outputArray, outputArray.Length + 1);
+            int lastIndex = outputArray.Length - 1;
+            outputArray[lastIndex] = inputArray[i];
+        }
+    }
+}
+
+static void PrintArray(string[] inputArray)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+
+        Console.Write($"{inputArray[i]}");
+        if (i != inputArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write(" ]");
+}
